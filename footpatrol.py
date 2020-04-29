2f2bd943678b15396d3f331ecb39002de0b867bf
lijinho <58988274+lijinho@users.noreply.github.com>
2020-04-28 22:06:15 -0400

    save database

--- footpatrol.py
+++ footpatrol.py
@@ -1,11 +1,18 @@
 from urllib.request import urlopen as uReq
 from urllib.request import Request
-
+from selenium import webdriver
+import requests
+headers = {
+	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
+}
 
 # request part
-headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36'}
-my_url = Request("https://www.footpatrol.com/campaign/New+In/brand/jordan/?facet-new=latest", headers=headers)
+my_url = requests.get("https://www.footpatrol.com/campaign/New+In/brand/jordan/?facet-new=latest",headers=headers)
+
+print(my_url.content)
+'''
 uClient=uReq(my_url)
 page_html=uClient.read()
 uClient.close()
 print(page_html)
+'''
